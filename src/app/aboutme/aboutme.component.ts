e3eb875e2f2c92826323b49eebf662e2c1549fac
Sahid Fakiry <s.fakiry23@gmail.com>
2024-05-26 20:38:22 +0200

    fix(whoami): stick footer to the bottom and add comments to component

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-
+/** Main Component to present myself */
 @Component({
   selector: 'app-aboutme',
   standalone: true,
@@ -8,13 +8,17 @@ import { Component, signal } from '@angular/core';
   styleUrl: './aboutme.component.scss',
 })
 export class AboutmeComponent {
+  /** Getter to calculate the age of someone who was born in 2004 */
   public get age(): number {
     return new Date().getUTCFullYear() - 2004;
   }
 
+  /** List of the word "Hello" in different languages */
   public helloMessages: string[] = ['Hallo', 'Hello', 'Bonjour'];
+  /** The current index of the chosen language, that can changes based on user interactions */
   public currentLanguageIndex = signal(0);
 
+  /** Changes the "Hello" index by rotating through the helloMessages List */
   public changeHelloMessage(): void {
     const newLanguageIndex =
       this.currentLanguageIndex() === this.helloMessages.length - 1
